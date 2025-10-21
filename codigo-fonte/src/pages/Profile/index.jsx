37b8cb32f1890ad23dfde64c51b88ea34d1a175f
Dangeles Lima <dlplima@hotmail.com>
2025-10-20 21:38:46 -0300

    feature: add number

--- codigo-fonte/src/pages/Profile/index.jsx
+++ codigo-fonte/src/pages/Profile/index.jsx
@@ -3,7 +3,7 @@ import Breadcrumb from "../../components/Breadcrumb";
 export default function Profile() {
   return (
     <div>
-      <h1>Perfil Público</h1>
+      <h1>Perfil Público 2</h1>
       <Breadcrumb />
     </div>
   );
