950eeffefda1208748242cc481eb862d2471e3f6
Gustavo Santiago <gustavosb845@gmail.com>
2025-10-20 22:07:58 -0300

    feature: test

--- codigo-fonte/src/pages/Profile/index.jsx
+++ codigo-fonte/src/pages/Profile/index.jsx
@@ -3,7 +3,7 @@ import Breadcrumb from "../../components/Breadcrumb";
 export default function Profile() {
   return (
     <div>
-      <h1>Perfil Público</h1>
+      <h1>Perfil Público 01</h1>
       <Breadcrumb />
     </div>
   );
