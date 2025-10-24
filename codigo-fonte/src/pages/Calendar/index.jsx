3b75a5b00997effb4ce788c0026f0dd91dbbf813
Yago Rodrigues <yago.dmoraes@gmail.com>
2025-10-23 21:35:33 -0300

    feat: add tag and non functional button

--- codigo-fonte/src/pages/Calendar/index.jsx
+++ codigo-fonte/src/pages/Calendar/index.jsx
@@ -1,10 +1,20 @@
 import Breadcrumb from "../../components/Breadcrumb";
 
 export default function Calendar() {
-  return (
+    return (
     <div>
       <h1>Agenda</h1>
       <Breadcrumb />
+      <div className="cal-actions">
+          <div className="cal-tags">
+            <button className="tag tag-note">📝 Lembrete</button>
+            <button className="tag tag-task">✅ Tarefa</button>
+            <button className="tag tag-event">🎉 Evento</button>
+          </div>
+
+          <button className="btn-primary">➕ Novo compromisso</button>
+      </div>
+      
     </div>
   );
 }
