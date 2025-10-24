2a1d3e950873432b9dbe8f7472436b7bb74d1ff9
YuriJardim <yuri.jardim212@gmail.com>
2025-10-23 22:22:59 -0300

    Customers: Update customer table
    Ainda preciso trocar os botões por ícones

--- codigo-fonte/src/pages/Customers/index.jsx
+++ codigo-fonte/src/pages/Customers/index.jsx
@@ -5,6 +5,36 @@ export default function Clientes() {
     <div>
       <h1>Clientes</h1>
       <Breadcrumb />
+      <fieldset>
+      <input type="text" placeholder="Digite o termo..." />
+      <button>Buscar</button>
+      </fieldset>
+        <button>Filtrar</button>
+          <button>Novo Cliente</button>
+     <table id="customersTable">
+      <thead>
+        <tr>
+          <th onclick="sortTable(0)">ID</th>
+          <th onclick="sortTable(1)">Cliente</th>
+          <th onclick="sortTable(2)">E-mail</th>
+          <th onclick="sortTable(3)">Contato</th>
+          <th onclick="sortTable(4)">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>João Silva</td>
+          <td>joao.silva@example.com</td>
+          <td>(11) 91234-5678</td>
+          <td>
+            <button>Excluir</button>
+            <button>Editar</button>
+            <button>Visualizar</button>
+          </td>
+        </tr>
+      </tbody>
+     </table>
     </div>
   );
 }
